d3fa4862a538a1ccfbab184bf8b46cbe6ace6f2d
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-02-01 14:45:40 +0530

    Create maximum element.c

---
+++ maximum element.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) {
+	int a[5],i,j,t;
+	for(i=1;i<=5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=1;i<=5;i++)
+	{
+		for(j=i+1;j<=5;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	printf("%d",a[5]);
+	return 0;
+}
+
